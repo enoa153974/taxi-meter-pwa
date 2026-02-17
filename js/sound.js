a4898a292633da931d9a72c5793e9c2c84142e7a
enoa153974 <aoi.153974@gmail.com>
2026-02-18 02:53:15 +0900

    所見更新

--- js/sound.js
+++ js/sound.js
@@ -1,35 +1,15 @@
 let meterSound = null;
 
-// 初期化
 export function initSound() {
     meterSound = new Audio('/sounds/meter_sound.wav');
     meterSound.preload = 'auto';
-
-    const unlock = () => {
-        meterSound.play()
-            .then(() => {
-                meterSound.pause();
-                meterSound.currentTime = 0;
-            })
-            .catch(err => {
-                console.warn("Sound unlock failed:", err);
-            });
-    };
-
-    // 全デバイス対応
-    document.addEventListener('pointerdown', unlock, { once: true });
 }
 
-// 再生
 export function playMeterSound() {
     if (!meterSound) {
-        console.warn("Sound not initialized");
-        return;
+        meterSound = new Audio('/sounds/meter_sound.wav');
     }
 
     meterSound.currentTime = 0;
-
-    meterSound.play().catch(err => {
-        console.warn("Play blocked:", err);
-    });
+    meterSound.play().catch(() => { });
 }
